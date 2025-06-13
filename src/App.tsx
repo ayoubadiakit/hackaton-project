1045fb65a10a1e07a24237fc2ae4e9b16d00363b
Ayouba DIAKITE <d.ayouba@its.lan>
2025-06-13 17:04:57 +0000

    Optimisation du code

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import NavigationBar from './components/NavigationBar.js';
 import Loading from './components/Loading.tsx';
 import Dashboard from './pages/Dashboard.tsx';
 import Footer from './components/Footer.tsx';
-import ProductSection from './components/ProductSection.tsx';
-import ProductPopulary from './components/ProductPopulary.tsx';
-import FeaturSection from './components/FeaturSection.tsx';
-import ClientSatisfait from './components/ClientSatisfait.tsx';
-import Investisseur from './components/Investisseur';
-
 function App() {
   return (
     <>
